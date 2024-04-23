bb0ed022e7fc230d4e2db52906276bde8eb812d3
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2024-04-22 20:38:59 -0400

    trying to use http

--- pages/index.jsx
+++ pages/index.jsx
@@ -39,6 +39,15 @@ export default function Home() {
       setSearchResults([]);
       setSelectedTrackFeatures(await response.json());
       document.getElementById("my_modal_3").close();
+      if (selectedTrackFeatures) {
+        fetch("http://localhost:5000/receive_data", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(selectedTrackFeatures),
+        });
+      }
     }
   }
 
